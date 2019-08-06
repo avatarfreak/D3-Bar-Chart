c5477c98d5f20a1c2e3adbaa954d1ad192ee970c
avatarfreak <avatarfreak>
2019-08-06 16:40:18 +0530

    initial commit

---
+++ READEME.md
@@ -0,0 +1,103 @@
+# Data Visualization Projects - Responsive Visualize Data with a Bar Chart
+
+- Fulfill the below user stories and get all of the tests to pass. Give it your own personal style.
+
+- You can use HTML, JavaScript, CSS, and the D3 svg-based visualization library. The tests require axes to be generated using the D3 axis property, which automatically generates ticks along the axis. These ticks are required for passing the D3 tests because their positions are used to determine alignment of graphed elements. You will find information about generating axes at [https://github.com/d3/d3/blob/master/API.md#axes-d3-axis](https://github.com/d3/d3/blob/master/API.md#axes-d3-axis). Required (non-virtual) DOM elements are queried on the moment of each test. If you use a frontend framework (like Vue for example), the test results may be inaccurate for dynamic content. We hope to accommodate them eventually, but these frameworks are not currently supported for D3 projects.
+- User Story #1: My chart should have a title with a corresponding `id="title"`.
+- User Story #2: My chart should have a g element x-axis with a corresponding `id="x-axis"`.
+- User Story #3: My chart should have a g element y-axis with a corresponding `id="y-axis"`.
+- User Story #4: Both axes should contain multiple tick labels, each with the corresponding `class="tick"`.
+- User Story #5: My chart should have a rect element for each data point with a corresponding `class="bar"` displaying the data.
+- User Story #6: Each bar should have the properties data-date and data-gdp containing date and GDP values.
+- User Story #7: The bar elements' data-date properties should match the order of the provided data.
+- User Story #8: The bar elements' data-gdp properties should match the order of the provided data.
+- User Story #9: Each bar element's height should accurately represent the data's corresponding GDP.
+- User Story #10: The data-date attribute and its corresponding bar element should align with the corresponding value on the x-axis.
+- User Story #11: The data-gdp attribute and its corresponding bar element should align with the corresponding value on the y-axis.
+- User Story #12: I can mouse over an area and see a tooltip with a corresponding `id="tooltip"` which displays more information about the area.
+- User Story #13: My tooltip should have a data-date property that corresponds to the data-date of the active area.
+  Here is the dataset you will need to complete this project: [https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json](https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json)
+
+## Important notes
+
+- This responsive visual graphic chart is built on top of **D3 version 5** library. First thing first, to achieve responisve chart, `window: resize event` is being used.
+
+  ```
+  window.addEventListener('resize', somefunction)
+  ```
+
+- **D3v5** substituted asynchronous callbacks with promises. Therefore, fetching external data is different from previous version.
+  In previous the data used to be fetched in this order
+
+  ```
+  var data = d3.json(‘data.json’, function(data){
+      //then do something with data
+      console.log(data)
+  });
+  ```
+
+- However, the promises in later version is slightly different from previous one. If, you have no idea what promises is all about? Kindly, do some research on `Javascript’s promises and fetch(url)`. The syntax in **D3v5** looks
+
+  ```
+  var data = d3.json(‘data.json’).then(function(data){
+      return (data);
+  }).catch(function(err){
+      console.log(err)
+  });
+  ```
+
+#### Project Structure
+
+```
+Project
+│
+│   index.html
+│   README.md
+│   package.json
+│   webpack.config.js
+└───src
+│   │
+│   └───assets
+|        └───img
+│           └───favicon.ico
+│       └───js
+│           └───index.js
+│           └───data.json
+|           └───bundle.js
+│       └───scss
+│            └───main.scss
+│
+│
+│
+└───dist
+```
+
+### Technologies used
+
+- webpack.
+- sass
+- ES6
+- svg
+- D3.js v5
+
+Clone this repo:
+
+```
+$ git clone https://github.com/avatarfreak/bar-chart
+```
+
+Install all dependancies:
+
+```
+$ npm install
+```
+
+Start the server:
+
+```
+$ npm run start:dev
+```
+
+#### Author
+
+- [avatarfreak](https://github.com/avatarfreak "avatarfreak")
---
+++ src/assets/js/index.js
@@ -0,0 +1,180 @@
+import "../scss/main.scss";
+import * as d3 from "d3";
+
+//fetching data using promises
+let dataset = d3.json("src/assets/js/data.json").then(res => res.data);
+dataset.then(data => {
+  //event listener for responsive behaviour
+  window.addEventListener("resize", render);
+  //initail rendering of pages
+  render();
+  //rendering function get fire when browser is resize
+  function render() {
+    //targeting div with class display__svg
+    const svg = d3.select(".display__svg");
+
+    //margin convention practice
+    const windowsize = {
+      height: document.querySelector(".display").clientHeight,
+      width: document.querySelector(".display").clientWidth,
+      margin: { top: 20, right: 100, bottom: 120, left: 100 }
+    };
+
+    //calling graph generator
+    drawchart(data, svg, windowsize);
+  }
+});
+
+//graph generator operation
+function drawchart(data, selection, props) {
+  //destructing
+  const { width, height, margin } = props;
+
+  //append svg element on page
+  let svg = selection.selectAll("svg").data([null]);
+  svg = svg
+    .enter()
+    .append("svg")
+    .merge(svg)
+    .attr("width", width)
+    .attr("height", height);
+
+  //calculate inner height
+  const innerHeight = height - margin.top - margin.bottom;
+
+  //calculate inner width
+  const innerWidth = width - margin.left - margin.right;
+
+  //barwidth
+  const barwidth = innerHeight / data.length;
+
+  //retrieving all the years(string) and converting js data format
+  const years = [...data.map(data => new Date(data[0]))];
+  // retrieving all the gdps
+  const gdp = [...data.map(data => data[1])];
+
+  //create x scale
+  const xScale = d3
+    .scaleTime()
+    .domain([d3.min(years), d3.max(years)])
+    .range([0, innerWidth]);
+
+  //creating responsive tick along the x-axis
+  const xticksdensity = 70;
+  //create an xaxis component with d3.axisbottom
+  const xaxis = d3.axisBottom(xScale).ticks(innerWidth / xticksdensity);
+
+  //call the xaxis in a group tag
+  let xaxisgroup = svg.selectAll("#x-axis").data([null]);
+  xaxisgroup = xaxisgroup
+    .enter()
+    .append("g")
+    .attr("id", "x-axis")
+    .merge(xaxisgroup)
+    .attr("transform", `translate(${margin.left},${innerHeight + margin.top})`)
+    .call(xaxis);
+
+  //create yScale
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(gdp)])
+    .range([innerHeight, 0]);
+  //creating responsive tick along the y-axis
+  const yticksdensity = 70;
+  //create an yaxis component with d3.axisLeft
+  const yaxis = d3.axisLeft(yScale).ticks(innerHeight / yticksdensity);
+  //call the xaxis in a group tag
+  let yaxisgroup = svg.selectAll("#y-axis").data([null]);
+  yaxisgroup = yaxisgroup
+    .enter()
+    .append("g")
+    .attr("id", "y-axis")
+    .merge(yaxisgroup)
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  yaxisgroup.call(yaxis);
+  //append labels in y axis
+  const yaxislabel = yaxisgroup.selectAll(".y-axis-label").data([null]);
+  yaxislabel
+    .enter()
+    .append("text")
+    .attr("class", "y-axis-label")
+    .merge(yaxislabel)
+    .attr("fill", "white")
+    .text("gross domestic product")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -innerHeight / 5)
+    .attr("y", -60);
+  
+    //creat linearscale
+  const linearscale = d3
+    .scaleLinear()
+    .domain([0, d3.max(gdp)])
+    .range([0, innerHeight]);
+
+  //creat newscaledgdp
+  let scaledgdp = gdp.map(data => linearscale(data));
+
+  //tootip for displaying information
+  const tooltip = selection
+    .append("div")
+    .attr("id", "tooltip")
+    .style("position", "absolute")
+    .style("padding", "5px 7px")
+    .style("border", "1px #333 solid")
+    .style("border-radius", "5px")
+    .style("opacity", "0");
+
+  //append rectangle to the svg
+  let rect = svg.selectAll("rect").data(scaledgdp);
+  rect = rect
+    .enter()
+    .append("rect")
+    .merge(rect)
+    .attr("class", "bar")
+    .attr("data-date", (d, i) => data[i][0])
+    .attr("data-gdp", (d, i) => data[i][1])
+    .attr("x", (d, i) => xScale(years[i]))
+    .attr("y", innerHeight)
+    .attr("width", barwidth)
+    .attr("height", 0)
+    .attr("fill", "orange")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+    .on("mouseover", function(d, i) {
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", "1");
+      tooltip
+        .html(
+          `
+            <label>year: <b>${data[i][0].substring(0, 4)}</b> </label> 
+            <br>
+            <label>$<b>${data[i][1]} billion</b></label> 
+            `
+        )
+        .attr("data-date", data[i][0])
+        .attr("data-gdp", data[i][1])
+        .style("left", `${d3.event.pagex}px`)
+        .style("top", `${d3.event.pagey}px`);
+      d3.select(this).attr("fill", "white");
+    })
+    .on("mouseout", function() {
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0);
+      d3.select(this).attr("fill", "orange");
+    });
+
+  //elastic animation
+  rect
+    .transition()
+    .ease(d3.easeLinear)
+    .duration(2000)
+    .ease(d3.easeElastic)
+    .attr("y", d => innerHeight - d)
+    .attr("height", d => d);
+}
+
+
+
